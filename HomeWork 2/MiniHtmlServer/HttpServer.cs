f6a7514f9ed704f6411ab9849dfd845f079ddae0
Wezirow A <aganiyazwezirow@gmail.com>
2025-10-05 12:57:24 +0300

    Add files via upload

---
+++ HomeWork 2/MiniHtmlServer/HttpServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using MiniHtmlServer.Shared;
+
+namespace MiniHtmlServer
+{
+    public class HttpServer
+    {
+        private HttpListener _listener;
+        private SettingsModel _config;
+
+        public HttpServer(SettingsModel config)
+        {
+            _config = config;
+        }
+
+        public void Start()
+        {
+            _listener = new HttpListener();
+            // BURASI YANLIŞTI → "server" yok, _listener kullanmalısın
+            _listener.Prefixes.Add($"http://{_config.Domain}:{_config.Port}/");
+            _listener.Start();
+            Receive();
+            Console.WriteLine("Сервер запущен.");
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            Console.WriteLine("Сервер остановлен.");
+        }
+
+        private void Receive()
+        {
+            _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+        }
+
+        private async void ListenerCallback(IAsyncResult result)
+        {
+            if (_listener.IsListening)
+            {
+                var context = _listener.EndGetContext(result);
+                var request = context.Request;
+
+                Console.WriteLine($"Запрос: {request.Url}");
+
+                string responseText = "<h1>Hello from server!</h1>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseText);
+
+                context.Response.ContentLength64 = buffer.Length;
+                await context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+                context.Response.OutputStream.Close();
+
+                Receive(); 
+            }
+        }
+    }
+}
---
+++ HomeWork 2/MiniHtmlServer/Shared/SettingsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniHtmlServer.Shared
+{
+    public class SettingsModel
+    {
+        public string PublicDirectoryPath { get; set; }
+        public string Domain { get; set; }
+        public int Port { get; set; }
+    }
+}
