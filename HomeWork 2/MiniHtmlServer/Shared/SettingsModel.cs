816802f8baa2140e752846eef7bf13c778093e04
Wezirow A <aganiyazwezirow@gmail.com>
2025-10-25 02:06:24 +0300

    Delete HomeWork 2/MiniHtmlServer directory

--- HomeWork 2/MiniHtmlServer/Shared/SettingsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniHtmlServer.Shared
-{
-    public class SettingsModel
-    {
-        public string PublicDirectoryPath { get; set; }
-        public string Domain { get; set; }
-        public int Port { get; set; }
-    }
-}
