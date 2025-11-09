e2504d11b95bec9a9011f00ac1753f1f06195fa3
Kardesim <aganiyazwezirow@gmail.com>
2025-11-09 15:50:06 +0300

    HomeWork 5

---
+++ HomeWork 5/MiniHttpServer/MiniHttpServer/Shared/FindFolder.cs
@@ -0,0 +1,39 @@
+﻿public static class FindFolder
+{
+    private static readonly Dictionary<string, string> folderPath = new()
+    {
+        {"gpt.html","public/gpt/gpt.html"},
+        {"gptstyle.css", "public/gpt/gptstyle.css"},
+        {"finger.html", "public/finger/finger.html"},
+        {"iam.jpg", "public/images/iam.jpg"},
+        {"forget.html", "public/loginpage/forget.html"},
+        {"index.html", "public/loginpage/index.html"},
+        {"register.html", "public/loginpage/register.html"},
+        {"style.css", "public/loginpage/style.css"},
+        {"olora.html", "public/olora/olora.html"},
+        {"olora.css", "public/olora/css/olora.css"},
+        {"circle.png", "public/olora/img/circle.png"},
+        {"Illustration.png", "public/olora/img/Illustration.png"},
+        {"File.zip","public/File.zip" },
+        {"bg.jpg", "public/images/bg.jpg"},
+        {"logo.png", "public/images/logo.png"},
+        {"Deepseek.jpg", "public/images/Deepseek.jpg"},
+        {"Gpt.jpg", "public/images/Gpt.jpg"},
+        {"in.jpg", "public/images/in.jpg"},
+        {"Github.jpg", "public/images/Github.jpg"},
+        {"GG.jpg", "public/images/GG.jpg"},
+        {"Plus.jpg", "public/images/Plus.jpg"},
+        {"youtube.jpg","public/images/youtube.jpg" },
+        {"404.html","public/404.html" }
+    };
+
+    public static string DicFind(string path)
+    {
+        return folderPath.TryGetValue(path, out string? value) ? value : null;
+    }
+
+    public static byte[] Code404 ()
+    {
+       return File.ReadAllBytes(DicFind("404.html"));
+    }
+}
---
+++ HomeWork 5/MiniHttpServer/MiniTemplateEngine/Core/GetPath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace MiniTemplateEngine.Core
+{
+    public class GetPath
+    {
+        public object? GetValueByPath(object obj, string path)
+        {
+            if (obj == null || string.IsNullOrEmpty(path))
+                return null;
+
+            if (obj is IDictionary dict && dict.Contains(path))
+                return dict[path];
+
+            var parts = path.Split('.');
+            object? current = obj;
+
+            foreach (var part in parts)
+            {
+                if (current == null)
+                    return null;
+
+                if (current is IDictionary d && d.Contains(part))
+                {
+                    current = d[part];
+                    continue;
+                }
+
+                var prop = current.GetType().GetProperty(part, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (prop == null)
+                    return null;
+
+                current = prop.GetValue(current);
+            }
+
+            return current;
+        }
+    }
+}
